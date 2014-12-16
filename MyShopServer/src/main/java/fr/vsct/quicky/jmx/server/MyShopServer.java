cb13ff728de949d6d2111e4fde326b0b7109fc36
Sylvain <sylvain.maillard6@gmail.com>
2014-12-16 01:14:15 +0100

    Add comments

--- MyShopServer/src/main/java/fr/vsct/quicky/jmx/server/MyShopServer.java
+++ MyShopServer/src/main/java/fr/vsct/quicky/jmx/server/MyShopServer.java
@@ -1,17 +1,9 @@
 package fr.vsct.quicky.jmx.server;
 
-import com.codahale.metrics.DefaultObjectNameFactory;
-import com.codahale.metrics.JmxReporter;
-import com.codahale.metrics.MetricRegistry;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import javax.management.ObjectName;
-import java.util.concurrent.TimeUnit;
 
 /**
  * MyShop Server
