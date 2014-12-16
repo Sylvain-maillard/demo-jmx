761487a8cfe0b5df879fcfd336a61be91d4c48fe
Sylvain <sylvain.maillard6@gmail.com>
2014-12-16 01:14:15 +0100

    Add comments

--- MyShopServer/src/main/java/fr/vsct/quicky/jmx/server/FrontEndServices.java
+++ MyShopServer/src/main/java/fr/vsct/quicky/jmx/server/FrontEndServices.java
@@ -24,33 +24,73 @@ public class FrontEndServices {
         this.dao = dao;
     }
 
-    @RequestMapping(value = "/products/prices/{min}/{max}")
-    public Collection<Product> allProductsBetweenPrices(@PathVariable(value = "min") int min, @PathVariable("max") int max) {
-        return dao.findProductBetween(min, max).values();
-    }
-
+    /**
+     * login to the system
+     *
+     * @param id user id.
+     * @return the customer informations
+     */
     @RequestMapping(value = "/login/{id}")
     public Customer login(@PathVariable("id") Integer id) {
         return dao.getCustomer(id);
     }
 
+    /**
+     * select products between min and max prices.
+     *
+     * @param min the min price
+     * @param max the max price
+     * @return a list of matching products.
+     */
+    @RequestMapping(value = "/products/prices/{min}/{max}")
+    public Collection<Product> allProductsBetweenPrices(@PathVariable(value = "min") int min, @PathVariable("max") int max) {
+        return dao.findProductBetween(min, max).values();
+    }
+
+    /**
+     * Add items to the logged user
+     *
+     * @param customerId the user id
+     * @param productId  the product to add
+     * @return the current basket.
+     */
     @RequestMapping(value = "/basket/{customerId}/add/{productId}")
     public Basket addToBasket(@PathVariable("customerId") int customerId, @PathVariable("productId") int productId) {
         return dao.addToBasket(customerId, productId);
     }
 
+    /**
+     * display the customer current basket.
+     *
+     * @param customerId the customer id.
+     * @return the basket
+     */
     @RequestMapping(value = "/basket/{customerId}")
     public Basket displayBasket(@PathVariable("customerId") int customerId) {
         return dao.getBasket(customerId);
     }
 
+    /**
+     * turn the customer basket into a order that could be paid.
+     * the basket is cleared after this operation.
+     *
+     * @param customerId user id.
+     * @return the new order.
+     */
     @RequestMapping(value = "/basket/{customerId}/order")
     public Order basketOrder(@PathVariable("customerId") int customerId) {
         return dao.saveOrder(customerId);
     }
 
+    /**
+     * display the order
+     *
+     * @param orderId the order id.
+     * @return the order.
+     */
     @RequestMapping(value = "/orders/{orderId}")
     public Order displayOrder(@PathVariable("orderId") int orderId) {
         return dao.getOrder(orderId);
     }
+
 }
